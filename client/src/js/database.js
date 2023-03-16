b25dc13378347e20760c198336267874bef17838
Rima Bhumbla <105192284+rbhumbla1@users.noreply.github.com>
2023-03-15 20:39:58 -0700

    added header as a default value to editor

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,14 +1,14 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('jateRB', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('jateRB')) {
+        console.log('jateRB database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('jateRB', { keyPath: 'id', autoIncrement: true });
+      console.log('jateRB database created');
     },
   });
 
@@ -16,13 +16,13 @@ const initdb = async () =>
 export const putDb = async (content) => {
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const contactDb = await openDB('jateRB', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readwrite');
+  const tx = contactDb.transaction('jateRB', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('jate');
+  const store = tx.objectStore('jateRB');
 
   // Use the .add() method on the store and pass in the content.
   const request = store.add({ content });
@@ -37,13 +37,13 @@ export const getDb = async () => {
   console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const contactDb = await openDB('jateRB', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readonly');
+  const tx = contactDb.transaction('jateRB', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('jate');
+  const store = tx.objectStore('jateRB');
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -6,13 +6,15 @@ export default class {
   constructor() {
     const localData = localStorage.getItem('content');
 
+    console.log("localdata", localData);
+
     // check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
     }
 
     this.editor = CodeMirror(document.querySelector('#main'), {
-      value: '',
+      value: 'header',
       mode: 'javascript',
       theme: 'monokai',
       lineNumbers: true,
@@ -22,10 +24,17 @@ export default class {
       tabSize: 2,
     });
 
+
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
+      if (data)
+        console.info('Loaded data from IndexedDB, injecting into editor');
+      else if (localData)
+        console.info('Loaded data from localStore, injecting into editor');
+      else if (header)
+        console.info('Loaded data from header, injecting into editor');
+
       this.editor.setValue(data || localData || header);
     });
 
