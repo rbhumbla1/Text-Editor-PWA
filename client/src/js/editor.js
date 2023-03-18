a31c40bb3c360c664bdaad2dd073b780b3a5b762
Rima Bhumbla <105192284+rbhumbla1@users.noreply.github.com>
2023-03-18 14:50:34 -0700

    debug in editor

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -30,6 +30,8 @@ export default class {
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
+
+      console.log("editor data", data);
       
       console.info('Loaded data from IndexedDB, injecting into editor')
 
